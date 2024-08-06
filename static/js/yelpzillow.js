a70b1a7fbac8663eac2ab6a9adbc53f637670296
Cameron Walker <txcwalker@gmail.com>
2024-08-05 22:54:53 -0500

    Updating path in yelpzillow.js

--- static/js/yelpzillow.js
+++ static/js/yelpzillow.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     function loadPlot(elementId, jsonFile) {
-        fetch(`/images/Housing_price/${jsonFile}`)
+        fetch(`/static/data/${jsonFile}`)
             .then(response => {
                 if (!response.ok) {
                     throw new Error(`Network response was not ok: ${response.statusText}`);
